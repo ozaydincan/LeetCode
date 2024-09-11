7aa271b25a84a6d14332319c5007d09bc8d4f5e8
CanOzaydin <ozaydincan.app@gmail.com>
2024-09-12 01:03:44 +0200

    12/09/2024 problem solution in Rust

---
+++ spiral_matrix_iv/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() :val(0),next(nullptr){}
+    ListNode(int x): val(x), next(nullptr){}
+    ListNode(int x, ListNode* next): val(x), next(next){}
+};
+
+
+class Solution{
+    public:
+    std::vector<std::vector<int>> spiralMatrix(int m, int n, ListNode* head){
+        while(top < m && )
+    }
+};
+
+
+int main(){
+    return 0;
+}
